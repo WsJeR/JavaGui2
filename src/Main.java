d8ff8a931431ad6de17b0883471ae8941860e517
vladimir <thesijer@gmail.com>
2023-06-05 22:29:55 +0300

    Введите пароль

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,83 @@
 import javax.swing.*;
 
 public class Main {
-    private static final String[] RESOLUTIONS = {"800x600", "1280x720", "1920x1080"};
-
     public static void main(String[] args) {
-        JFrame resolutionDialog = new JFrame("Выбор разрешения");
-        resolutionDialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        resolutionDialog.setSize(300, 150);
-        resolutionDialog.setLocationRelativeTo(null);
+        showWelcomeDialog();
+    }
 
-        JComboBox<String> resolutionComboBox = new JComboBox<>(RESOLUTIONS);
-        resolutionComboBox.setSelectedIndex(0);
+    private static void showWelcomeDialog() {
+        int option = JOptionPane.showConfirmDialog(null, "Добро пожаловать! Хотите зарегистрироваться в программе?",
+                "Регистрация", JOptionPane.YES_NO_OPTION);
+        if (option == JOptionPane.YES_OPTION) {
+            showLoginDialog();
+        } else {
+            System.exit(0);
+        }
+    }
 
-        JButton okButton = new JButton("OK");
-        okButton.addActionListener(e -> {
-            String selectedResolution = (String) resolutionComboBox.getSelectedItem();
-            String[] dimensions = selectedResolution.split("x");
-            int width = Integer.parseInt(dimensions[0]);
-            int height = Integer.parseInt(dimensions[1]);
+    private static void showLoginDialog() {
+        String login = "";
+        while (login.length() <= 5 || login.contains(" ")) {
+            login = JOptionPane.showInputDialog(null, "Введите логин (длина должна быть больше 5 символов и не должен содержать пробелов):",
+                    "Логин", JOptionPane.PLAIN_MESSAGE);
+            if (login == null) {
+                System.exit(0);
+            }
+        }
+        showPasswordDialog();
+    }
 
-            JFrame frame = new JFrame("Окно с выбранным разрешением");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(width, height);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+    private static void showPasswordDialog() {
+        String password = "";
+        while (!isValidPassword(password)) {
+            JPasswordField passwordField = new JPasswordField();
+            Object[] message = {"Введите пароль (длина должна быть больше 8 символов, не должен содержать пробелов, " +
+                    "должен содержать хотя бы одну цифру и хотя бы одну букву):", passwordField};
+            int option = JOptionPane.showConfirmDialog(null, message, "Пароль", JOptionPane.OK_CANCEL_OPTION);
+            if (option == JOptionPane.CANCEL_OPTION) {
+                System.exit(0);
+            }
+            password = String.valueOf(passwordField.getPassword());
+        }
+        showConfirmPasswordDialog(password);
+    }
 
-            resolutionDialog.dispose();
-        });
+    private static void showConfirmPasswordDialog(String password) {
+        String confirmPassword = "";
+        while (!password.equals(confirmPassword)) {
+            JPasswordField passwordField = new JPasswordField();
+            Object[] message = {"Повторите пароль:", passwordField};
+            int option = JOptionPane.showConfirmDialog(null, message, "Повтор пароля", JOptionPane.OK_CANCEL_OPTION);
+            if (option == JOptionPane.CANCEL_OPTION) {
+                System.exit(0);
+            }
+            confirmPassword = String.valueOf(passwordField.getPassword());
+        }
+        showRegistrationSuccessDialog();
+    }
 
-        JPanel panel = new JPanel();
-        panel.add(resolutionComboBox);
-        panel.add(okButton);
-        resolutionDialog.getContentPane().add(panel);
+    private static void showRegistrationSuccessDialog() {
+        JOptionPane.showMessageDialog(null, "Регистрация успешно завершена!", "Успешная регистрация",
+                JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
 
-        resolutionDialog.setVisible(true);
+    private static boolean isValidPassword(String password) {
+        if (password.length() < 8 || password.contains(" ")) {
+            return false;
+        }
+        boolean hasLetter = false;
+        boolean hasDigit = false;
+        for (char ch : password.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                hasLetter = true;
+            } else if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+            if (hasLetter && hasDigit) {
+                return true;
+            }
+        }
+        return false;
     }
 }
